b80f89b6ec88ed8043cf702a7f0badd1ecb884ac
DeeptanshuSanghani1 <deeptanshusanghani@gmail.com>
2023-09-23 12:22:31 -0400

    Created user Class and implemented an overridable method for Id.

---
+++ StackSwapApplication/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+using System.ComponentModel.DataAnnotations.Schema;
+using StackSwapApplication.Models.BaseEntities;
+
+namespace StackSwapApplication.Models
+{
+    public class User : BaseEntity
+    {
+        private uint _id;
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override uint Id { get => _id; set => SetProperty(ref _id, value); }
+
+
+        public uint GetId => _id;
+
+
+
+
+    }
+}
