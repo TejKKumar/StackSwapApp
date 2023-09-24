d8acf5ddb8e905949a3e2ba50e0918636aef858c
DeeptanshuSanghani1 <deeptanshusanghani@gmail.com>
2023-09-23 20:45:10 -0400

    Added more properties to User Class

--- StackSwapApplication/Models/User.cs
+++ StackSwapApplication/Models/User.cs
@@ -7,12 +7,23 @@ namespace StackSwapApplication.Models
     public class User : BaseEntity
     {
         private uint _id;
+        private string _name;
+        private string _email;
+        private string _password;
+        private string _userName;
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public override uint Id { get => _id; set => SetProperty(ref _id, value); }
 
 
         public uint GetId => _id;
+
+        public string Name { get => _name; set= _name; }
+        public string Email { get => _email; set = _email; }
+        public string Password { get => _password; set = _password;}
+        public string UserName { get => _userName; set = _userName;}
+
+
 
 
 
