869433c5d2616cd3f7cf799e4eeaf6ec2f7d256a
Tejas Kumar <kumateja@sheridancollege.ca>
2023-09-27 12:00:45 -0400

    Changed Registration of IDataService to singleton instead of transient

--- StackSwapApplication/Program.cs
+++ StackSwapApplication/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddTransient<IDataService, DataRepository>();
+builder.Services.AddSingleton<IDataService, DataRepository>();
 
 
 var app = builder.Build();
