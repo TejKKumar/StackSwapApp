7f75f01589fdced6a19c5abcaaac7699dcae0182
Imran Yafith <imranyafith@gmail.com>
2023-09-28 12:41:02 -0400

    Added Champion.cs and DefaultCard.cs models

---
+++ StackSwapApplication/Models/Champion.cs
@@ -0,0 +1,8 @@
+﻿using StackSwapApplication.Models.BaseEntities;
+
+namespace StackSwapApplication.Models;
+
+public class Champion : BaseCharacter
+{
+    public uint GetID => base.Id;
+}
---
+++ StackSwapApplication/Models/DefaultCard.cs
@@ -0,0 +1,12 @@
+﻿using StackSwapApplication.Models.BaseEntities;
+
+namespace StackSwapApplication.Models;
+
+public class DefaultCard : BaseCard
+{
+
+    public Champion champion { get; set; }
+    public uint ChampId => champion.GetID;
+    public uint GetId => base.GetID;
+
+}
