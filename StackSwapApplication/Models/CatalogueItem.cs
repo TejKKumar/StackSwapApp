9a53fa4bb869d17d2b126108f5b548a384856a9e
Tejas Kumar <kumateja@sheridancollege.ca>
2023-11-06 17:24:59 -0500

    Created CatalogueItem model, and a catalogue service with a static list, this will be used to allow users to purchase cards

---
+++ StackSwapApplication/Models/CatalogueItem.cs
@@ -0,0 +1,27 @@
+﻿namespace StackSwapApplication.Models
+{
+    public class CatalogueItem
+    {
+        private string _champion;
+        private Tier _tier;
+        private double _damage;
+        private double _magic;
+        private double _health;
+
+        
+        public CatalogueItem(string champion, Tier tier, double damage, double magic, double health)
+        {
+            _champion = champion;
+            _tier = tier;
+            _damage = damage;
+            _magic = magic;
+            _health = health;
+        }
+
+        public string GetChampion { get => _champion; }
+        public Tier GetTier { get => _tier; }
+        public double GetDamage { get => _damage; }
+        public double GetMagic { get => _magic; }
+        public double GetHealth { get => _health; }
+    }
+}
---
+++ StackSwapApplication/Services/CatalogueRepository.cs
@@ -0,0 +1,58 @@
+﻿using StackSwapApplication.Models;
+
+namespace StackSwapApplication.Services
+{
+    public class CatalogueRepository : ICatalogueService
+    {
+        public static List<CatalogueItem> catalogueItems = new List<CatalogueItem>()
+        {
+            new CatalogueItem("Ash",Tier.Tier1,250,890,1200),
+            new CatalogueItem("Bellrus", Tier.Tier1, 300, 150, 2300),
+            new CatalogueItem("GraveWard", Tier.Tier1, 720, 1300, 900),
+            new CatalogueItem("D3=N", Tier.Tier1, 320, 450, 900),
+            new CatalogueItem("Raven", Tier.Tier1, 1200, 1400, 60),
+
+            new CatalogueItem("Ash",Tier.Tier2,300,890,1200),
+            new CatalogueItem("Bellrus", Tier.Tier2, 350, 150, 2300),
+            new CatalogueItem("GraveWard", Tier.Tier2, 770, 1300, 900),
+            new CatalogueItem("D3=N", Tier.Tier2, 370, 450, 900),
+            new CatalogueItem("Raven", Tier.Tier2, 1250, 1400, 60),
+
+            new CatalogueItem("Ash",Tier.Tier3,350,890,1200),
+            new CatalogueItem("Bellrus", Tier.Tier3, 400, 150, 2300),
+            new CatalogueItem("GraveWard", Tier.Tier3, 820, 1300, 900),
+            new CatalogueItem("D3=N", Tier.Tier3, 420, 450, 900),
+            new CatalogueItem("Raven", Tier.Tier3, 1300, 1400, 60),
+
+            new CatalogueItem("Ash",Tier.Tier4,400,890,1200),
+            new CatalogueItem("Bellrus", Tier.Tier4, 450, 150, 2300),
+            new CatalogueItem("GraveWard", Tier.Tier4, 870, 1300, 900),
+            new CatalogueItem("D3=N", Tier.Tier4, 470, 450, 900),
+            new CatalogueItem("Raven", Tier.Tier3, 1350, 1400, 60),
+
+            new CatalogueItem("Ash",Tier.Tier5,450,890,1200),
+            new CatalogueItem("Bellrus", Tier.Tier5, 500, 150, 2300),
+            new CatalogueItem("GraveWard", Tier.Tier5, 920, 1300, 900),
+            new CatalogueItem("D3=N", Tier.Tier5, 520, 450, 900),
+            new CatalogueItem("Raven", Tier.Tier5, 1400, 1400, 60),
+        };
+
+
+        public void CreateCard(out Card newCard, CatalogueItem cItem)
+        {
+            newCard = new Card
+            {
+                Champion = cItem.GetChampion,
+                CardTier = cItem.GetTier,
+                Damage = cItem.GetDamage,
+                Magic = cItem.GetMagic,
+                Health = cItem.GetHealth,
+            };
+        }
+
+        public IEnumerable<CatalogueItem> GetCatalogueItems()
+        {
+            return catalogueItems.AsEnumerable();
+        }
+    }
+}
