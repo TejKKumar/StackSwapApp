4f029e4bf3b22d440134bebdb7cfb8fb84c581b7
Imran Yafith <imranyafith@gmail.com>
2023-11-10 14:51:23 -0500

    Create interface for purchase transaction

---
+++ StackSwapApplication/Services/PurchaseTransactionService.cs
@@ -0,0 +1,11 @@
+﻿using StackSwapApplication.Models;
+
+namespace StackSwapApplication.Services
+{
+    public interface PurchaseTransactionService
+    {
+
+        public void AddPurchase(User user, List<Card> cards);
+
+    }
+}
