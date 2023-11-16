dee128ff5b78cb75a97db32f67d0e43b4d8823f9
Tejas Kumar <kumateja@sheridancollege.ca>
2023-11-16 11:58:47 -0500

    Created Action methods in TradeController so that logged in users can view that cards of another user they select.

--- StackSwapApplication/Controllers/TradeController.cs
+++ StackSwapApplication/Controllers/TradeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StackSwapApplication.Models;
 using StackSwapApplication.Services;
 using StackSwapApplication.ViewModels;
 
@@ -6,9 +7,9 @@ namespace StackSwapApplication.Controllers
 {
     public class TradeController : Controller
     {
-        public ICardService _repo;
+        public IDataService _repo;
         public IUserSession _userSession;   
-        public TradeController(ICardService repo, IUserSession userSession)
+       public TradeController(IDataService repo, IUserSession userSession)
         {
             _repo = repo;
             _userSession = userSession;
@@ -25,7 +26,7 @@ namespace StackSwapApplication.Controllers
                 }
                 else
                 {
-                    var cards = _repo.GetAllCards();
+                    var cards = _repo.GetCards;
                     return View(cards);
                 }
             }
@@ -35,5 +36,41 @@ namespace StackSwapApplication.Controllers
             }
 
         }
+
+        [HttpGet] //Get Method to get a view of all the users 
+        public IActionResult ViewUsers() 
+        {
+
+            var userList = _repo.GetUsers;
+            return View(userList);
+        }
+
+        [HttpPost] //Post action called to view a particular user that redirects to another view // This used to validate the user Id before redirecting
+        public IActionResult ViewUsers(uint Id)
+        {
+            return Id != 0 ? RedirectToAction("ViewUserCards","Trade", new {Id = Id}) : View();
+        }
+
+
+        [HttpGet] //Gets The Page to View User Cards to Initiate a Trade
+       public IActionResult ViewUserCards(uint Id)
+        {
+            ViewCardsVM vm = new ViewCardsVM();
+
+            var Seller = _repo.GetUsers.Single(u => u.Id == Id);
+            var SellerCards = _repo.GetCards.Where(c => c.OwnerID == Id).AsEnumerable();
+
+            TradeUser? Buyer = _userSession.GetCurrentUser();
+            var BuyerCards = _repo.GetCards.Where(c => c.Id == Seller.Id);
+
+            vm.Buyer = Buyer;
+            vm.BuyerCards = BuyerCards;
+
+            vm.Seller = Seller;
+            vm.SellerCards = SellerCards;
+            return View(vm);
+        }
+
+
     }
 }
---
+++ StackSwapApplication/ViewModels/ViewCardsVM.cs
@@ -0,0 +1,13 @@
+﻿using StackSwapApplication.Models;
+
+namespace StackSwapApplication.ViewModels
+{
+    public class ViewCardsVM
+    {
+        public TradeUser Buyer { get; set; }
+        public IEnumerable<Card> BuyerCards {  get; set; } 
+
+        public TradeUser Seller { get; set; }
+        public IEnumerable<Card> SellerCards { get; set; }
+    }
+}
